4d0a7a3943b5583b348539b41d56f537b00e5ecd
Ian Tewksbury <itewksbu@redhat.com>
2014-08-21 17:30:57 -0400

    Add jboss_admin::pattern::security_domain_with_authentication_classic. Add test for jboss_admin::pattern::security_domain_with_authentication_classic. Update README.md to include information about new pattern types.

---
+++ tests/pattern/quickstart_servlet_security.pp
@@ -0,0 +1,16 @@
+jboss_admin::server{ 'example':
+  base_path => '/opt/jboss'
+}
+
+jboss_admin::pattern::security_domain_with_authentication_classic { 'servlet-security-pattern-quickstart':
+  server         => example,
+  cache_type     => 'default',
+  code           => 'Database',
+  flag           => 'required',
+  module_options => {
+    dsJndiName      => "java:jboss/datasources/ServletSecurityDS",
+    principalsQuery => "SELECT PASSWORD FROM USERS WHERE USERNAME = ?",
+    rolesQuery      => "SELECT R.NAME, \'Roles\' FROM USERS_ROLES UR INNER JOIN ROLES R ON R.ID = UR.ROLE_ID INNER JOIN USERS U ON U.ID = UR.USER_ID WHERE U.USERNAME = ?"
+  },
+  ensure         => present,
+}
