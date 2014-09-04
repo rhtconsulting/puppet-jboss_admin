3059c608be1addb86169812ab240c08e348f7b31
Ian Tewksbury <itewksbu@redhat.com>
2014-09-04 11:30:23 -0400

    add jboss_admin::dependencies define for including required outside dependincies of this module

---
+++ manifests/dependencies.pp
@@ -0,0 +1,15 @@
+# = Define jboss_admin::dependecnies
+#
+# Optional define to include all of the required outside dependencies of the
+# jboss_admin module
+#
+define jboss_admin::dependencies(
+) {
+  # install required packages and required gems
+  package { ['ruby-devel', 'gcc']: } ->
+  package { ['treetop', 'json']:
+    provider => gem
+  }
+
+  Package['Treetop'] -> Jboss_admin::Server<||>
+}
