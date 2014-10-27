7d1bcf4de8ca61428d69a1c067d00a27223b48f0
Ian Tewksbury <itewksbu@redhat.com>
2014-10-27 11:16:08 -0400

    update jboss_admin::module type to allow for an entire directory to be managed by the module rather then just a single file.

--- manifests/module.pp
+++ manifests/module.pp
@@ -3,6 +3,13 @@
 # Creates the file structure for a module, including the module.xml, 
 # and manages the resource jar.
 #
+# = Parameters
+#
+# [*is_directory*]
+#  true if the *resource_path* is a directory so recusivly copy all directory contents to the module.
+#  false if the *resource_path* is a single file so copy that single file to the module.
+#  Defaults to false.
+#
 # = Example
 #
 #  jboss_admin::module{ 'com.mysql':
@@ -13,8 +20,9 @@
 #  }
 #
 define jboss_admin::module (
-  $resource_path,
   $server,
+  $resource_path,
+  $is_directory = false,
   $dependencies = [],
   $namespace    = $name,
   $ensure       = present
@@ -26,26 +34,45 @@ define jboss_admin::module (
   $module_path      = "${server_base_path}/modules"
   $namespace_path   = regsubst($namespace, '\.', '/', 'G')
   $dir_path         = "${module_path}/${namespace_path}/main"
-  $resource_name    = inline_template('<%= File.basename(@resource_path) %>')
-  
+
 
   File {
     owner => $server_user,
     group => $server_group
   }
 
-  $parent_dirs = child_directory_list($module_path, "${namespace_path}/main")
+  # create parent directories to module
+  $parent_dirs = child_directory_list($module_path, "${namespace_path}")
   @file { $parent_dirs:
     ensure => directory,
     tag    => ['moduledir', $namespace]
   }
   File <| tag == moduledir and tag == $namespace |>
+  
+  # if the module contents is a directory
+  # else if is single file, such as a jar
+  if $is_directory {
+    $resource_name = '.'
 
-  file { "${dir_path}/${resource_name}":
-    ensure  => file,
-    source  => $resource_path
+    file { "${dir_path}":
+      ensure  => directory,
+      source  => $resource_path,
+      recurse => true,
+    }
+  } else {
+    $resource_name = inline_template('<%= File.basename(@resource_path) %>')
+
+    file { "${dir_path}":
+      ensure  => directory,
+      tag    => ['moduledir', $namespace]
+    }
+    file { "${dir_path}/${resource_name}":
+      ensure  => file,
+      source  => $resource_path
+    }
   }
 
+  # create the module deffinition file
   file { "${dir_path}/module.xml":
     ensure   => file,
     content  => template('jboss_admin/module.erb')
