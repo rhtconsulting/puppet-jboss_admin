cf412332b4e817103608ccce141c56c21d7f060c
Chris Pitman <cpitman1@gmail.com>
2015-04-01 10:48:20 -0400

    Reverting change to timeout command

--- lib/puppet/util/cli_execution.rb
+++ lib/puppet/util/cli_execution.rb
@@ -30,7 +30,7 @@ module Puppet::Util::CliExecution
 
     delete_nil = Proc.new { |k, v| v.kind_of?(Hash) ? (v.delete_if(&delete_nil); nil) : v.nil? }
 
-    output = execute [ '--timeout', "#{server['cli_execute_timeout_minutes']}m", cli_path, '--connect','--controller=' + server['management_ip'] + ':' + server['management_port'], '--file=' + command_file.path], {:failonfail => failonfail, :combine => true}
+    output = execute [ 'timeout', "#{server['cli_execute_timeout_minutes']}m", cli_path, '--connect','--controller=' + server['management_ip'] + ':' + server['management_port'], '--file=' + command_file.path], {:failonfail => failonfail, :combine => true}
 
     if batch
       return {'outcome' => 'success'} if output =~ /The batch executed successfully/
